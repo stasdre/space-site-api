027df5a81ebbc9f49a1bd2f2864309be50c7708a
Stanislav Drehval <stasdre@gmail.com>
2020-12-18 10:31:12 +0200

    comment cookies https

--- src/utils/setTokenCookie.js
+++ src/utils/setTokenCookie.js
@@ -1,9 +1,9 @@
 export const setTokenCookie = (res, token) => {
   const cookieOptions = {
-    //domain: process.env.AUDIENCE,
+    domain: process.env.AUDIENCE,
     expires: new Date(Date.now() + process.env.REFRESH_TOKEN_EXPIRES * 1000),
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
+    //secure: process.env.NODE_ENV === 'production',
   };
   res.cookie('__rt', token, cookieOptions);
 };
