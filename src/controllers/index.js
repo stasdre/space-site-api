dacaff8ae7d6462d38ffd765e26c003d9d28fb8f
Stanislav Drehval <stasdre@gmail.com>
2021-01-05 16:22:51 +0200

    add dotenv settings

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,6 @@
 export * as Auth from './auth';
 export * as Works from './works';
-export * as WorkTypes from './WorkTypes';
+export * as WorkTypes from './workTypes';
 export * as Service from './services';
 export * as Langs from './langs';
 export * as Prices from './prices';
