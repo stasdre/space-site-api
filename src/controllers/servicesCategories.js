3fe71e796991e5804d2e05ed2153bf25a97fc971
Stanislav Drehval <stasdre@gmail.com>
2021-01-14 14:57:05 +0200

    endpoint for get all categories with services

--- src/controllers/servicesCategories.js
+++ src/controllers/servicesCategories.js
@@ -1,8 +1,11 @@
+import { async } from 'crypto-random-string';
 import db from '../models';
 
 const ServiceCategories = db.serviceCategories;
 const ServiceCategoriesData = db.serviceCategoriesData;
 const Langs = db.lang;
+const Service = db.service;
+const ServicesData = db.servicesData;
 
 const getAll = async (req, res) => {
   const { lang } = req.params;
@@ -34,4 +37,57 @@ const getAll = async (req, res) => {
   }
 };
 
-export { getAll };
+const getAllWidthServices = async (req, res) => {
+  const { lang } = req.params;
+  const dataCategories = [];
+  try {
+    const categories = await ServiceCategories.findAll({
+      attributes: ['id'],
+    });
+
+    const data = await ServiceCategoriesData.findAll({
+      attributes: [['ServiceCategoryId', 'id'], 'name'],
+      where: {
+        ServiceCategoryId: categories.map((item) => item.id),
+      },
+      include: [
+        {
+          model: Langs,
+          attributes: ['id'],
+          where: { code: lang },
+        },
+      ],
+      raw: true,
+    });
+
+    for (const item of data) {
+      const dataServices = await ServicesData.findAll({
+        attributes: ['name', 'url'],
+        include: [
+          {
+            model: Service,
+            attributes: ['id'],
+            where: {
+              active: 1,
+              ServiceCategoryId: item.id,
+            },
+          },
+          {
+            model: Langs,
+            attributes: ['id'],
+            where: { code: lang },
+          },
+        ],
+        raw: true,
+      });
+
+      dataCategories.push({ ...item, services: dataServices.map((item) => item) });
+    }
+
+    res.status(200).send({ data: dataCategories });
+  } catch (error) {
+    res.status(500).send({ message: 'Something went wrong!!!' });
+  }
+};
+
+export { getAll, getAllWidthServices };
--- src/routers/servicesCategories.js
+++ src/routers/servicesCategories.js
@@ -4,6 +4,7 @@ import { ServicesCategories } from '../controllers';
 
 export const router = express.Router();
 
+router.get('/services/:lang', ServicesCategories.getAllWidthServices);
 router.get('/:lang*?', ServicesCategories.getAll);
 
 export { router as categories };
